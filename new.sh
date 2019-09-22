02a72db11cad93654b7dbfb40603e399b28039fa
khml <43922054+khml@users.noreply.github.com>
2019-09-22 11:21:22 +0900

    set command quasar dev

--- new.sh
+++ new.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-if [ "${#}" == "0" ]; then
-    echo "Error! given ${#} args. At leaset, need 1 args for AppName(dirname)"
-    echo "${0} AppName"
-    exit 0
-fi
-
-docker-compose run quasar quasar create $@
+docker-compose run quasar quasar create .
 
